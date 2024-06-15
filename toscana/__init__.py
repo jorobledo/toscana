e61fc356703f5741998fc10dbf9a2215c29742d6
Jose Robledo <j.robledo@fz-juelich.de>
2024-06-15 08:28:25 +0200

    added docstring

--- toscana/__init__.py
+++ toscana/__init__.py
@@ -1,1 +1,11 @@
-from .toscana import *
+"""
+ToScAna
+=======
+
+Package for the basic data treatment of disordered material diffractograms at D4.
+
+How to use the documentation
+----------------------------
+Documentation is available in docstrings provided with the code example: help(toscana).
+"""
+from . import toscana
